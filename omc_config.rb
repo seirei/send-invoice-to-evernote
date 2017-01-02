604f0b86d77c92eb3d084d13246f220f3558ef4d
Mori Tomohito <seirei@me.com>
2017-01-02 12:45:51 +0900

    Change URL for omc login site.

--- omc_config.rb
+++ omc_config.rb
@@ -3,7 +3,7 @@ require 'mechanize'
 require 'uri'
 
 # Define OMC URL for download invoices
-OMC_LOGIN_URL = "https://ca.omc-card.co.jp/member/omcplus_login.html"
-OMC_DOWNLOAD_URL = "https://ca.omc-card.co.jp/member/xt_pdf_download.asp"
+OMC_LOGIN_URL = "https://ca.cedyna.co.jp/member/omcplus_login.html"
+OMC_DOWNLOAD_URL = "https://ca.cedyna.co.jp/member/xt_pdf_download.asp"
 OMC_USER = "your user id" # login id for omc
 OMC_PASS = "user password" # login password for omc
